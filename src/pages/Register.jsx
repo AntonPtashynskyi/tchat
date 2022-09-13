d7be7d0de15e53119458ffe55b4f0784f218c190
Anton Ptashynskyi <a.ptashynskyi@gmail.com>
2022-09-13 20:22:41 +0200

    create Sing-up page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./pageStyle.scss";
+import Add from "../media/img/upoload.png";
+
+export const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">T-chat</span>
+        <span className="pageTitle">register</span>
+        <form className="loginForm">
+          <input type="text" placeholder="name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <label htmlFor="file">
+            <img src={Add} alt="upload" className="uploadImg" />
+            <span>add avatar</span>
+          </label>
+          <input type="file" id="file" style={{ display: "none" }} />
+          <button type="submit" onClick={(e) => e.preventDefault()}>
+            Sign-up
+          </button>
+        </form>
+        <p>
+          You do have an account? <span>Log In</span>
+        </p>
+      </div>
+    </div>
+  );
+};
