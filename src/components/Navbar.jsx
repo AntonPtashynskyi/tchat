16c947bc3309a2abf9f70eea2ad4ce2a7dd3228f
Anton Ptashynskyi <a.ptashynskyi@gmail.com>
2022-09-17 21:08:26 +0200

    add search section styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,10 @@ function Navbar() {
     <div className="navbar">
       <span className="logo">T-chat</span>
       <div className="user">
-        <img src="" alt="" />
+        <img src="https://media.istockphoto.com/photos/portrait-of-handsome-latino-african-man-picture-id1007763808?k=20&m=1007763808&s=612x612&w=0&h=q4qlV-99EK1VHePL1-Xon4gpdpK7kz3631XK4Hgr1ls=" alt="" />
         <span>Anton</span>
         <button>logout</button>
+        
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../style/base.style.scss";
 import Navbar from "./Navbar";
+import Search from "./Search"
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <Navbar />
+      <Search/>
     </div>
   );
 }
